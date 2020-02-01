bab0c7a33878b54cb9842af64e9dc7a615c2edb9
Tom Clarke <tom@sinejunky.com>
2020-02-01 11:33:00 +0000

    Added empty chl_opengl module.

---
+++ chl_opengl/chl_opengl.h
@@ -0,0 +1,49 @@
+/**
+ *  chl_opengl.h
+ *
+ *  MIT License
+ *
+ *  Copyright (c) 2018, Tom Clarke
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+/*******************************************************************************
+ The block below describes the properties of this module, and is read by
+ the Projucer to automatically generate project code that uses it.
+ For details about the syntax and how to create or use a module, see the
+ JUCE Module Format.txt file.
+
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:               chl_opengl
+  vendor:           chl
+  version:          0.0.1
+  name:             CHL OpenGL classes
+  description:      Classes to assist with OpenGL rendering via JUCE
+  license:          MIT
+
+  dependencies:     juce_opengl
+
+ END_JUCE_MODULE_DECLARATION
+
+*******************************************************************************/
+
+#pragma once
